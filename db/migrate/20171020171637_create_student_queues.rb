b23f4bbff3ecb0067d08ce8209b4f85e4e56ff10
Haggai Kaunda <haggaikaunda@berkeley.edu>
2017-10-20 17:22:58 +0000

    adds fields to migration for StudentQueues.

--- db/migrate/20171020171637_create_student_queues.rb
+++ db/migrate/20171020171637_create_student_queues.rb
@@ -1,8 +1,18 @@
 class CreateStudentQueues < ActiveRecord::Migration
   def change
     create_table :student_queues do |t|
+      
+      #entered by student, visible on tutor form
+      
+      t.integer :student_sid #should be foreign key.
+      t.string :course
+      t.integer :tutor_sid
+      t.time :start_time
 
-      t.timestamps null: false
+      #*** what will be the values of status? -- "in progress", "waiting" 
+      t.boolean :waiting?
+      t.timestamps  #sort by create time.
+      
     end
   end
 end
