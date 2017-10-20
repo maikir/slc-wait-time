b5488ee4dfcc42ec73c563b3e0ab1e71a5fac24e
Jennifer B <jbe@berkeley.edu>
2017-10-20 22:45:48 +0000

    Add enter/leave line pages

--- app/controllers/student_requests_controller.rb
+++ app/controllers/student_requests_controller.rb
@@ -1,18 +1,27 @@
 class StudentRequestsController < ApplicationController
 	
   def wait_time
-	  @student_request = StudentRequest.find(params[:id])
-	  @wait_position = @student_request.wait_pos
-	  @wait_time = @wait_position * 30
-      
+    @student_request = StudentRequest.find(params[:id])
+	@wait_position = @student_request.wait_pos
+	@wait_time = @wait_position * 30
   end
     
   def new
-	    # render new template	
+    # render new template	
   end
 	
   def create
-	    # place holder
+    # place holder
   end
+
+  def confirm
+    #wait in line
+  end
+
+  def leave
+    #don't wait in line
+    @student_request = StudentRequest.find(params[:id])
     
+  end
+
 end
