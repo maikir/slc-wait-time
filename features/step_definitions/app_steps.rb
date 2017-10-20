812b8f340664a5d47c6300f757348801f1108701
Jennifer B <jbe@berkeley.edu>
2017-10-20 23:53:38 +0000

    Add step 'Then I should not be on..'

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -88,3 +88,12 @@ end
 Then /^I should see a wait time of (.*)$/ do |page_name|
   visit path_to(page_name)
 end
+
+Then /^(?:|I )should not be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should != path_to(page_name)
+  else
+    assert_not_equal path_to(page_name), current_path
+  end
+end
