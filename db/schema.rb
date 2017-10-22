11445892da794cbab468b65e04a63f82e8e60ba1
Alex Yang <alexyang@Alexs-MacBook-Pro.local>
2017-10-22 16:58:30 -0700

    schema reflecting new model

--- db/schema.rb
+++ db/schema.rb
@@ -38,29 +38,9 @@ ActiveRecord::Schema.define(version: 20171022195019) do
   create_table "student_queues", force: :cascade do |t|
     t.integer  "student_id"
     t.string   "course"
-    t.integer  "tutor_sid"
-    t.time     "start_time"
-    t.boolean  "waiting?"
+    t.time     "join_time"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "student_requests", force: :cascade do |t|
-    t.string   "last_name"
-    t.string   "first_name"
-    t.string   "sid"
-    t.string   "request_type"
-    t.string   "course"
-    t.string   "residency_status"
-    t.boolean  "transfer_student"
-    t.boolean  "concurrency_student"
-    t.string   "tutor"
-    t.time     "start_time"
-    t.time     "end_time"
-    t.text     "notes"
-    t.string   "status"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
   end
 
   create_table "students", force: :cascade do |t|
@@ -74,7 +54,7 @@ ActiveRecord::Schema.define(version: 20171022195019) do
   end
 
   create_table "tutor_work_days", force: :cascade do |t|
-    t.integer  "tutor_sid"
+    t.integer  "tutor_id"
     t.integer  "start_time"
     t.integer  "end_time"
     t.string   "num_students"
