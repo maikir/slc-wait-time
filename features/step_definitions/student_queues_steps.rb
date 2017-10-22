3e642a31e8fdde8e0a4a53060b6d7aadacb17c21
Haggai Kaunda <haggaikaunda@berkeley.edu>
2017-10-21 17:27:52 -0700

    fixes indentation inconsistencies in student_queues_stpes.rb

--- features/step_definitions/student_queues_steps.rb
+++ features/step_definitions/student_queues_steps.rb
@@ -1,14 +1,14 @@
 
 Given /^"([^"]*)" "([^"]*)" is on the wait time page$/ do |first_name, last_name|
-    sid = 123456
-    steps %Q{
-        Given I am on the home page
-        Then I fill in "student_first_name" with "#{first_name}"
-        And I fill in "student_last_name" with "#{last_name}"
-        And I fill in "student_sid" with "#{sid}"
-        And I click on "form_submit"
-        Then I should be on the wait time page for "#{first_name}" "#{last_name}"
-    }
+  sid = 123456
+  steps %Q{
+    Given I am on the home page
+    Then I fill in "student_first_name" with "#{first_name}"
+    And I fill in "student_last_name" with "#{last_name}"
+    And I fill in "student_sid" with "#{sid}"
+    And I click on "form_submit"
+    Then I should be on the wait time page for "#{first_name}" "#{last_name}"
+  }
 end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
@@ -18,18 +18,20 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
 end
 
 Given /^"([^"]*)" "([^"]*)" is already in line$/ do |first_name, last_name| 
-    steps %Q{
-        Given "Jennifer" "Be" is on the wait time page
-        And she clicks on "YES"
-        Given "Jennifer" "Be" is on the wait time page
-    }
+  steps %Q{
+    Given "#{first_name}" "#{last_name}" is on the wait time page
+    And she clicks on "YES"
+    Given "#{first_name}" "#{last_name}" is on the wait time page
+  }
 end
 
 # Then /^(?:she|he?) should be on the home page/ do |page_name| 
 #     steps %Q{ Then I should be on the "#{page_name}"}
 # end
 
+
+
 And /^(?:she|he?) clicks on "(.*)"$/ do |button| 
-    steps %Q{ When follow "#{button}"}
+  steps %Q{ When follow "#{button}"}
 end
 
