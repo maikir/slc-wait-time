f76b6397d191d184b3e187c15f13f831687babff
Haggai Kaunda <haggaikaunda@berkeley.edu>
2017-10-22 14:34:59 -0700

    defines tutor#queue_to_session that creates an active session from a student_queue. This will be useful when removing students off the queue and adding them to the list of students currently being seen.

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -2,4 +2,27 @@ class Tutor < ActiveRecord::Base
   self.primary_key = :sid
   has_many :drop_in_histories
   has_one :active_session
+
+  #moves a student from the queue to the active session.
+  def queue_to_session(student_queue)
+    if student_queue.instance_of? StudentQueue
+      session_data = remove_unwanted(student_queue)
+      time_now = Time.now.in_time_zone('Pacific Time (US & Canada)')
+      session_data[:start_time] = time_now
+      session_data[:wait_time] = (session_data[:start_time] - time_now) / 60
+      self.create_active_session(session_data)
+    else
+      throw ArgumentError(%Q{expected student_queue but got "#{student_queue}"})
+    end
+  end
+
+  private
+    def remove_unwanted(student_queue)
+      queue_data = student_queue.attributes
+      remove = %w(created-at, updated_at)
+      remove.each do |value.to_sym|
+        queue_data.delete(value)
+      end
+      queue_data
+    end
 end
