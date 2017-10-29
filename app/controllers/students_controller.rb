a935f962d8178dd31f181fc8d5d075576440944f
Haggai Kaunda <haggaikaunda@berkeley.edu>
2017-10-29 14:29:45 -0700

    adds students_controller.rb and changes sign up page from student_queue#{new, create} to student#{new, create} respectively

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+  def new
+    #render new template
+  end
+
+  def create
+    #create student if they are not already in the database,
+  end
+
+  def sign_up
+    #direct the request based on what type of appointment the student is here for.
+  end
+
+end
